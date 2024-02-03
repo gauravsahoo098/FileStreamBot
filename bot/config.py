8106ea21ec01b3ba6751b5cd3b83bac06ac38547
gauravsahoo123 <158731701+gauravsahoo123@users.noreply.github.com>
2024-02-03 16:19:37 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ class Telegram:
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
     BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "sahoo_file_to_link_bot")
     BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6262640035:AAFfPSewiIypD4DUBvg2VrOoRGEA7m50bq8")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002065064286))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
