589b672661b9ec23a5bb008e04fa1f68a3e3ff23
gauravsahoo098 <159449508+gauravsahoo098@users.noreply.github.com>
2024-02-26 19:51:02 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,14 +5,14 @@ class Telegram:
     API_HASH = env.get("TELEGRAM_API_HASH", "262418f5ebe0a599821a5802a90ee1a5")
     OWNER_ID = int(env.get("OWNER_ID", 2014953230))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "2014953230").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "sahoo_private_file_bot")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6664876906:AAGarnBEDyu3QkrjjeOaprZxWPjsxZUdkM4")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002120701827))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "sahoovpsbot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6590978998:AAFxHZcqaRmDUeZ64mk9pCYy-n08bYVyAtI")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002040564082))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "https://sahoofilmbot-537959ddf190.herokuapp.com")
-    BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
+    BASE_URL = env.get("BASE_URL", "https://212.224.88.31")
+    BIND_ADDRESS = env.get("BIND_ADDRESS", "212.224.88.31")
     PORT = int(env.get("PORT", 8080))
 
 # LOGGING CONFIGURATION
