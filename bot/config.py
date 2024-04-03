4b2c1656230367b023b267c0cdb72135cdf22279
gauravsahoo098 <159449508+gauravsahoo098@users.noreply.github.com>
2024-04-04 04:25:55 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,7 +12,7 @@ class Telegram:
 
 class Server:
     BASE_URL = env.get("BASE_URL", "https://uclaadhar.cloud/")
-    BIND_ADDRESS = env.get("BIND_ADDRESS", "https://uclaadhar.cloud/")
+    BIND_ADDRESS = env.get("BIND_ADDRESS", "154.41.252.55")
     PORT = int(env.get("PORT", 80))
 
 # LOGGING CONFIGURATION
